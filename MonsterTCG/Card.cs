a7869cc1e85404f08cf1921708ebdfffca9f82e2
Wolfgang Mair <Wolfgang_Mair@Riam.at>
2020-10-01 11:54:08 +0200

    Klassendiagramm erstellt

---
+++ MonsterTCG/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonsterTCG
+{
+    public abstract class Card
+    {
+        public int name
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
---
+++ MonsterTCG/MonsterCard.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonsterTCG
+{
+    public class MonsterCard
+    {
+        public int health
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int damage
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
