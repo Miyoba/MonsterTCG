f3db27816b5a857e55cac19ab952a0606be8c99d
Wolfgang Mair <wolfgang.mair912@gmail.com>
2020-10-19 03:00:57 +0200

    Testing structure prepared

---
+++ MonsterTCG_Tests/DeckTest.cs
@@ -0,0 +1,20 @@
+using MonsterTCG;
+using NUnit.Framework;
+
+
+namespace MonsterTCG_Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+        Assert.Pass();
+        }
+    }
+}
