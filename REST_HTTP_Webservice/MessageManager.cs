9ed240310eb70abc718858d31a7c31406ee0dd67
Wolfgang Mair <wolfgang.mair912@gmail.com>
2020-11-12 00:18:31 +0100

    MessageManager created and tests prepared for further development.

---
+++ REST_HTTP_Webservice/MessageManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REST_HTTP_Webservice
+{
+    public class MessageManager
+    {
+        public MessageManager(HttpPackage package)
+        {
+
+        }
+
+        public string ReadMessage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ListMessages()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SaveMessage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string UpdateMessage()
+        {
+            throw new NotImplementedException();
+        }
+        public string DeleteMessage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ REST_HTTP_Webservice_Tests/MessageManagerTest.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using REST_HTTP_Webservice;
+
+namespace REST_HTTP_Webservice_Tests
+{
+    public class MessageManagerTest
+    {
+        [Test]
+        public void TestMessageManager_ReadMessage()
+        {
+            //TODO
+            // arrange
+            
+            // act
+            
+            // assert
+            
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestMessageManager_ListMessages()
+        {
+            //TODO
+            // arrange
+            
+            // act
+            
+            // assert
+            
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestMessageManager_SaveMessage()
+        {
+            //TODO
+            // arrange
+            
+            // act
+            
+            // assert
+            
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestMessageManager_UpdateMessage()
+        {
+            //TODO
+            // arrange
+            
+            // act
+            
+            // assert
+            
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestMessageManager_DeleteMessage()
+        {
+            //TODO
+            // arrange
+            
+            // act
+            
+            // assert
+            
+            Assert.Pass();
+        }
+    }
+}
